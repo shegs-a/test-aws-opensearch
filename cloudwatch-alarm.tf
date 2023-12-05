88d09a35fb28b64ae688705da690d180cff97269
Segun Samuel <shegs-a@users.noreply.github.com>
2023-12-05 23:19:55 +0100

    include SNS in the Cloudwatch alarm

--- cloudwatch-alarm.tf
+++ cloudwatch-alarm.tf
@@ -1,12 +1,18 @@
-resource "aws_cloudwatch_metric_alarm" "prod-alarm" {
-  alarm_name                = "op-observability-cloudwatch-alarm"
+resource "aws_cloudwatch_metric_alarm" "prod-alarm-2" {
+  alarm_name                = "op-observability-cloudwatch-alarm-2"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 2
-  metric_name               = "OpenSearchUtilization"
+  evaluation_periods        = 1
+  metric_name               = "OpenSearchClusterUsedSpace"
   namespace                 = "AWS/ES"
-  period                    = 28800
-  statistic                 = "Average"
-  threshold                 = 0.80
+  period                    = 60
+  statistic                 = "Maximum"
+  
+  threshold                 = 0.8
   alarm_description         = "This metric monitors opensearch utilization and raises an alarm once the utilization hits 80%"
-  insufficient_data_actions = []
+  actions_enabled           = "true"
+  alarm_actions             = [arn:aws:sns:us-east-1:467194453264:Default_CloudWatch_Alarms_Topic:6de8dac7-5bb4-4ac6-81b4-1a722a54b0d3]
+  ok_actions                = [arn:aws:sns:us-east-1:467194453264:Default_CloudWatch_Alarms_Topic:6de8dac7-5bb4-4ac6-81b4-1a722a54b0d3]
+
+  insufficient_data_actions = [arn:aws:sns:us-east-1:467194453264:Default_CloudWatch_Alarms_Topic:6de8dac7-5bb4-4ac6-81b4-1a722a54b0d3]
+
 }
